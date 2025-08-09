2dd99eb5eee805efd0c16f38a095e40888b07bc1
fatimanotron <fatimanotron@gmail.com>
2025-08-09 14:46:44 +0430

    refactor prompt messeges in post page(free-section)

--- src/components/free-sections/resume-sharing-notice.tsx
+++ src/components/free-sections/resume-sharing-notice.tsx
@@ -4,22 +4,25 @@ import Button from "~/components/button";
 
 function ResumeSharingNotice() {
   return (
-    <section className="bg-[#050615] px-[10px] lg:px-[30px] w-full py-[27px] text-[12px] lg:text-[14px] gap-y-[10px]  grid grid-cols-6 lg:grid-cols-12 grid-rows-3 ">
-      <div className="bg-crust row-start-1 row-end-3 rounded-[10px] w-[40px] h-[40px] lg:h-[48px] lg:w-[48px]  flex items-center justify-center">
+    <section className="bg-[#050615] flex flex-col px-[10px] text-text lg:px-[30px] w-full py-[27px] text-[12px] lg:text-[14px] ">
+     <section className="flex gap-[10px]">
+      <div className="bg-crust  rounded-[10px] w-[40px] h-[40px] lg:h-[48px] lg:w-[48px]  flex items-center justify-center">
         <LuRepeat1 className=" w-[20px] h-[20px] text-maroon" />
       </div>
       <Text
-        className="w-[285px] lg:w-[800px] row-start-1 row-end-3 lg:text-[16px] lg:col-start-2
-             flex  lg:col-end-11  col-start-2 col-end-6 lg:mr-[10px]"
+        className="w-full lg:text-[16px] 
+             flex lg:mr-[10px]"
       >
         متوجه شدیم. در حال حاضر مایل به اشتراک گذاری هایلایت ها و یادداشت هاتون
         نیستید! اما نگران نباشید میتونید با کلیک بر روی دکمه زیر اشتراک گذاری رو
         از سر بگیرید.
       </Text>
-
-      <Button className="w-[102px] h-[23px] bg-blue  font-bold col-start-6 justify-self-end self-end row-start-3 lg:col-start-12 ">
+      </section>
+<section className="flex justify-end">
+      <Button className="w-[102px]  h-[23px] bg-blue font-bold  ">
         از سرگیری
       </Button>
+      </section>
     </section>
   );
 }
