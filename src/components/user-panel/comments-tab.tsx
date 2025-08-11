8bce6f18ac68093c40c20d772bea2536bb53715a
fatimanotron <fatimanotron@gmail.com>
2025-08-11 18:55:41 +0330

    implement notes and highlights page in user panel

--- src/components/user-panel/comments-tab.tsx
+++ src/components/user-panel/comments-tab.tsx
@@ -12,117 +12,117 @@ import ThreeDotts from "~/assets/images/threeDotts.svg";
 import Button from "~/components/button";
 
 
-export default function CommentsTabUserPanel(){
+export default function CommentsTabUserPanel() {
 
-    return(
-        <section className="flex flex-col gap-[10px]">
+  return (
+    <section className="flex flex-col gap-[10px]">
 
-<span className="p-[10px] w-full flex justify-between items-center bg-mantle rounded-[10px]">
+      <span className="p-[10px] w-full flex justify-between items-center bg-mantle rounded-[10px]">
 
-<p className="text-[12px] text-subtext-0">در اینجا شما نظرات رو خواهید دید میتوانید برای پاسخ دادن از markdown برای استایل دادن به نظراتتون استفاده کنید.</p>
-<Close className="bg-crust" />
-</span>
+        <p className="text-[12px] text-subtext-0">در اینجا شما نظرات رو خواهید دید میتوانید برای پاسخ دادن از markdown برای استایل دادن به نظراتتون استفاده کنید.</p>
+        <Close className="bg-crust" />
+      </span>
 
- <article className=" w-full flex flex-col px-[20px] ">
-      <header className="flex justify-between  rounded-tr-[10px] rounded-tl-[10px] p-[10px]  bg-crust">
-        <div className="flex justify-center gap-[10px] items-center">
-          <Image
-            src={Profile}
-            alt="farhan"
-            className="rounded-full h-[25px] w-[25px]"
-          />
-          <Text className="text-[14px] text-subtext-0 font-bold">فرهان</Text>
-        </div>
-        <div className="flex items-center gap-[10px]">
-          <span className="text-[12px]">2+</span>
-          <Button className="bg-teal w-[28px] h-[28px] rounded-[5px] flex justify-center items-center">
-            <IncreaseArrow />
-          </Button>
-          <Button className="bg-maroon flex justify-center items-center w-[28px] h-[28px] rounded-[5px]">
-            <DecreaseArrow />
-          </Button>
-          <Button className="bg-  text-subtext-0">
-            <ThreeDotts />
-          </Button>
-        </div>
-      </header>
+      <article className=" w-full flex flex-col px-[20px] ">
+        <header className="flex justify-between  rounded-tr-[10px] rounded-tl-[10px] p-[10px]  bg-crust">
+          <div className="flex justify-center gap-[10px] items-center">
+            <Image
+              src={Profile}
+              alt="farhan"
+              className="rounded-full h-[25px] w-[25px]"
+            />
+            <Text className="text-[14px] text-subtext-0 font-bold">فرهان</Text>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <span className="text-[12px]">2+</span>
+            <Button className="bg-teal w-[28px] h-[28px] rounded-[5px] flex justify-center items-center">
+              <IncreaseArrow />
+            </Button>
+            <Button className="bg-maroon flex justify-center items-center w-[28px] h-[28px] rounded-[5px]">
+              <DecreaseArrow />
+            </Button>
+            <Button className="bg-  text-subtext-0">
+              <ThreeDotts />
+            </Button>
+          </div>
+        </header>
 
-      <section className="text-[12px] text-text bg-[#101122]  rounded-b-[10px] py-[10px] gap-y-[15px] flex flex-col ">
-        <section className="bg-surface-0  w-full h-auto flex justify-between text-text py-[13px] px-[10px] text-[14px]  ">
-          <p className=" ">در پاسخ به : من به نظرم فلان هست </p>
-        
+        <section className="text-[12px] text-text bg-[#101122]  rounded-b-[10px] py-[10px] gap-y-[15px] flex flex-col ">
+          <section className="bg-surface-0  w-full h-auto flex justify-between text-text py-[13px] px-[10px] text-[14px]  ">
+            <p className=" ">در پاسخ به : من به نظرم فلان هست </p>
+
+          </section>
+          <p className="px-[10px]">
+            سلام من این هایلایت ها و نوت ها رو برای خودم نوشته بودم و گفتم شاید
+            بدرد دیگران هم بخوره خلاصه آره گفتم اینجا بزارم شاید به بقیه هم کمک
+            کنه
+          </p>
+          <section className="flex justify-between px-[10px]">
+            <Button className="bg- text-text flex items-center gap-[5px]">
+              <CommentsIcon />
+              پاسخ دهید
+            </Button>
+          </section>
         </section>
-        <p className="px-[10px]">
-          سلام من این هایلایت ها و نوت ها رو برای خودم نوشته بودم و گفتم شاید
-          بدرد دیگران هم بخوره خلاصه آره گفتم اینجا بزارم شاید به بقیه هم کمک
-          کنه
-        </p>
-        <section className="flex justify-between px-[10px]">
-          <Button className="bg- text-text flex items-center gap-[5px]">
-            <CommentsIcon />
-            پاسخ دهید
-          </Button>
+      </article>
+
+
+
+      <article className=" w-full flex flex-col px-[20px] ">
+        <header className="flex justify-between  rounded-tr-[10px] rounded-tl-[10px] p-[10px]  bg-crust">
+          <div className="flex justify-center gap-[10px] items-center">
+            <Image
+              src={Profile}
+              alt="farhan"
+              className="rounded-full h-[25px] w-[25px]"
+            />
+            <Text className="text-[14px] text-subtext-0 font-bold">فرهان</Text>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <span className="text-[12px]">2+</span>
+            <Button className="bg-teal w-[28px] h-[28px] rounded-[5px] flex justify-center items-center">
+              <IncreaseArrow />
+            </Button>
+            <Button className="bg-maroon flex justify-center items-center w-[28px] h-[28px] rounded-[5px]">
+              <DecreaseArrow />
+            </Button>
+            <Button className="bg-  text-subtext-0">
+              <ThreeDotts />
+            </Button>
+          </div>
+        </header>
+
+        <section className="text-[12px] text-text bg-[#101122]  rounded-b-[10px] py-[10px] gap-y-[10px] flex flex-col ">
+          <section className="bg-surface-0  w-full h-auto flex justify-between text-text py-[13px] px-[10px] text-[14px]  ">
+            <p className=" ">در پاسخ به : من به نظرم فلان هست </p>
+
+          </section>
+          <p className="px-[10px]">
+            سلام من این هایلایت ها و نوت ها رو برای خودم نوشته بودم و گفتم شاید
+            بدرد دیگران هم بخوره خلاصه آره گفتم اینجا بزارم شاید به بقیه هم کمک
+            کنه
+          </p>
+          <section className="flex gap-[8px] px-[10px] ">
+            <Button className="bg- text-text flex items-center gap-[5px]">
+              <CommentsIcon />
+              پاسخ دهید
+            </Button>
+            <Button className="bg- text-text flex items-center gap-[5px]">
+              <Close className="bg- h-[13px] w-[13px]" />
+              لغو پاسخ
+            </Button>
+
+          </section>
+          <div className="px-[10px] text-[14px] flex flex-col gap-[10px]">
+            <TextInput className="h-[52px] text-text text-[14px]" placeholder="کامنت خود را بنویسید ..." />
+            <Button className="w-full h-[52px] bg-blue text-crust font-bold">ارسال نظر</Button>
+          </div>
         </section>
-      </section>
-    </article>
 
 
+      </article>
 
-<article className=" w-full flex flex-col px-[20px] ">
-      <header className="flex justify-between  rounded-tr-[10px] rounded-tl-[10px] p-[10px]  bg-crust">
-        <div className="flex justify-center gap-[10px] items-center">
-          <Image
-            src={Profile}
-            alt="farhan"
-            className="rounded-full h-[25px] w-[25px]"
-          />
-          <Text className="text-[14px] text-subtext-0 font-bold">فرهان</Text>
-        </div>
-        <div className="flex items-center gap-[10px]">
-          <span className="text-[12px]">2+</span>
-          <Button className="bg-teal w-[28px] h-[28px] rounded-[5px] flex justify-center items-center">
-            <IncreaseArrow />
-          </Button>
-          <Button className="bg-maroon flex justify-center items-center w-[28px] h-[28px] rounded-[5px]">
-            <DecreaseArrow />
-          </Button>
-          <Button className="bg-  text-subtext-0">
-            <ThreeDotts />
-          </Button>
-        </div>
-      </header>
+    </section>
 
-      <section className="text-[12px] text-text bg-[#101122]  rounded-b-[10px] py-[10px] gap-y-[10px] flex flex-col ">
-        <section className="bg-surface-0  w-full h-auto flex justify-between text-text py-[13px] px-[10px] text-[14px]  ">
-          <p className=" ">در پاسخ به : من به نظرم فلان هست </p>
-        
-        </section>
-        <p className="px-[10px]">
-          سلام من این هایلایت ها و نوت ها رو برای خودم نوشته بودم و گفتم شاید
-          بدرد دیگران هم بخوره خلاصه آره گفتم اینجا بزارم شاید به بقیه هم کمک
-          کنه
-        </p>
-        <section className="flex gap-[8px] px-[10px] ">
-          <Button className="bg- text-text flex items-center gap-[5px]">
-            <CommentsIcon />
-            پاسخ دهید
-          </Button>
-     <Button className="bg- text-text flex items-center gap-[5px]">
-            <Close className="bg- h-[13px] w-[13px]" />
-            لغو پاسخ
-          </Button>
-
-        </section>
-<div className="px-[10px] text-[14px] flex flex-col gap-[10px]">
-<TextInput className="h-[52px] text-text text-[14px]" placeholder="کامنت خود را بنویسید ..." />
-<Button className="w-full h-[52px] bg-blue text-crust font-bold">ارسال نظر</Button>
-</div>
-      </section>
-
-
-    </article>
-
-</section>
-        
-    )
+  )
 }
