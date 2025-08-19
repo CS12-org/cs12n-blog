73b5f5acf685292213705d5f44509671dc5a55af
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-08-20 00:18:51 +0330

    refactor: update styles and clean up markup in HighlightMessage component

--- src/components/posts/comment-messages/highlight-message.tsx
+++ src/components/posts/comment-messages/highlight-message.tsx
@@ -11,8 +11,8 @@ import Button from "~/components/button";
 
 function HighlightMessege() {
   return (
-    <article className=" w-full flex flex-col ">
-      <header className="flex justify-between  lg:rounded-tr-full lg:rounded-br-full rounded-tl-[10px] rounded-tr-[10px] p-[10px]  bg-crust">
+    <article className="w-full flex flex-col">
+      <header className="flex justify-between rounded-l-[10px] rounded-r-[24px] p-[10px] bg-crust">
         <div className="flex justify-center gap-[10px] items-center">
           <Image
             src={Profile}
@@ -24,16 +24,13 @@ function HighlightMessege() {
         <div className="flex items-center gap-[10px]">
           <span className="text-[12px]">2+</span>
           <Button className="bg-teal w-[28px] h-[28px] rounded-[5px] flex justify-center items-center">
-            {" "}
-            <IncreaseArrow   className="w-[20px] h-[10px]"/>
+            <IncreaseArrow className="w-[20px] h-[10px]" />
           </Button>
           <Button className="bg-maroon flex justify-center items-center w-[28px] h-[28px] rounded-[5px]">
-            {" "}
-            <DecreaseArrow   className="w-[20px] h-[10px]"/>{" "}
+            <DecreaseArrow className="w-[20px] h-[10px]" />
           </Button>
-          <Button className="bg-  text-subtext-0">
-            {" "}
-            <ThreeDotts />{" "}
+          <Button className="bg-surface-1 text-subtext-0">
+            <ThreeDotts />
           </Button>
         </div>
       </header>
@@ -46,17 +43,15 @@ function HighlightMessege() {
         <section className="flex justify-between">
           <div className="flex gap-[5px]">
             <Button className="p-[10px] justify-center items-center text-subtext-0  bg-mantle h-[36px] flex gap-[7.5px]">
-              {" "}
-              <Highlighter className="h-[28px] w-[28px]"/> هایلایت ها
+              <Highlighter className="h-[28px] w-[28px]" /> هایلایت ها
             </Button>
             <Button className="p-[10px] justify-center items-center text-subtext-0  bg-mantle h-[36px] flex gap-[7.5px]">
-              {" "}
-              <Notes className="h-[28px] w-[28px]"/> یادداشت ها
+              <Notes className="h-[28px] w-[28px]" /> یادداشت ها
             </Button>
           </div>
           <Button className="bg- text-overlay-1 flex items-center gap-[5px]">
             12
-            <Comments  className="w-[29px] h-[29px]" />
+            <Comments className="w-[29px] h-[29px]" />
           </Button>
         </section>
       </section>
