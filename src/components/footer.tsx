9071a2c0653bdbf4db5dd7ae77e7ee44f9849fa8
fatimanotron <fatimanotron@gmail.com>
2025-08-24 11:42:53 +0330

    refactore mobile navbar by buttons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,10 +6,10 @@ import About from "~/assets/images/aboutus.svg"
 export default function Footer(){
     return(
         <section className="bg-crust lg:hidden justify-between flex rounded-t-[11px] px-[50px] pt-[20px] pb-[10px] gap-[20px] text-overlay-1 text-[12px]">
-        <section className="flex flex-col items-center gap-[5px]"><About className="w-[22px] h-[22px]" /> درباره ما</section>    
-         <section className="flex flex-col items-center gap-[5px]" >  <Home className="w-[22px] h-[22px]" /> خونه</section> 
-           <section  className="flex flex-col items-center gap-[5px]"><Telephone className="w-[22px] h-[22px]" /> تماس با ما</section> 
-          <section  className="flex flex-col items-center gap-[5px]"> <Help className="w-[22px] h-[22px]" /> کمک!</section> 
+        <button className="flex flex-col items-center gap-[5px]"><About className="w-[22px] h-[22px]" /> درباره ما</button>    
+         <button className="flex flex-col items-center gap-[5px]" >  <Home className="w-[22px] h-[22px]" /> خونه</button> 
+           <button  className="flex flex-col items-center gap-[5px]"><Telephone className="w-[22px] h-[22px]" /> تماس با ما</button> 
+          <button  className="flex flex-col items-center gap-[5px]"> <Help className="w-[22px] h-[22px]" /> کمک!</button> 
         </section>
     )
 }
