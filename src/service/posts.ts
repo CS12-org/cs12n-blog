ae93c72ca0cdb7cc5ace088d4787f48c76e181d2
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-08-29 00:37:37 +0330

    fix: add missing api in posts

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -27,8 +27,75 @@ export const getPostBySlug = async (slug: string): Promise<Post> => {
   try {
     const res = await axios.get(`/api/posts/get-by-slug/${slug}`);
     return res.data;
-  } catch (err: any) {
+  } catch (err: unknown) {
     console.log(err);
     throw new Error("خطا در گرفتن پست");
   }
 };
+
+// ----------------------------------------------------------------------------
+
+export type GetPostsParams = {
+  page: number;
+  pageSize: number;
+}
+
+export type GetPostsResult = {
+  data: {
+    id: number;
+    description: string;
+    title: string;
+    content: string;
+    clap: number;
+    slug: string;
+    createdAt: string;
+    narrator: {
+      url: string;
+    } | null;
+    user: {
+      email: string;
+      username: string;
+    } | null;
+    tags: {
+      id: number;
+      title: string;
+      slug: string;
+    }[];
+    category: {
+      id: number;
+      slug: string;
+      title: string;
+    };
+    featured_image: {
+      id: number;
+      width: number;
+      height: number;
+      url: string;
+    };
+    slide_image:
+      | {
+          id: number;
+          width: number;
+          height: number;
+          url: string;
+        }[]
+      | null;
+  }[];
+  meta: {
+    pagination: {
+      page: number;
+      pageSize: number;
+      total: number;
+      pageCount: number;
+    };
+  };
+}
+
+/**
+ * Fetches posts from the API.
+ *
+ * @param params - Parameters for the request.
+ * @returns A promise that resolves to the posts data.
+ */
+export const getPosts = (params: GetPostsParams) =>
+  axios.get<GetPostsResult>("/api/posts/feed", { params });
