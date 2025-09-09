563a3432298d556c2852150d60d8d96cfb02f101
mohammadhosseintazaroie <m.h.tazaroie753@gmail.com>
2025-09-09 14:11:00 +0330

    fix linter bug with .next foldr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/**/*",
+      "node_modules/**/*",
+      "*.config.js",
+      "*.config.mjs",
+      "*.config.ts",
+      "next-env.d.ts",
+      "svgr-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-unsafe-function-type": "warn",
+      "@typescript-eslint/no-wrapper-object-types": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
