22d7e4e6a3d713f166ddfc15cfc924c9d7b43735
mohammadhosseintazaroie <m.h.tazaroie753@gmail.com>
2025-09-10 19:21:52 +0330

    fix lint and type errors

--- src/app/(blog)/user-panel/page.tsx
+++ src/app/(blog)/user-panel/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client";
 
 import Image from "next/image";
 import Cs12Logo from "~/assets/images/cs12-logo.svg";
@@ -7,237 +6,215 @@ import PlusSign from "~/assets/images/plus-sign.svg";
 import Profile from "~/assets/images/user-profile.png";
 import Button from "~/components/button";
 import { Text } from "~/components/react-aria-components";
-import { SettingCheckboxOption,
-  SettingCheckboxGroup} from "~/components/user-panel/setting-checkbox";
+import {
+  SettingCheckboxGroup,
+  SettingCheckboxOption,
+} from "~/components/user-panel/setting-checkbox";
 import { TextInput } from "~/components/user-panel/text-field";
 import MyRadioGroup from "~/components/user-panel/theme-setting";
-import { FormProvider, useForm } from "react-hook-form";
-import { useState } from "react";
-
-
-
-
 
 function UserPanel() {
-  const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
-const {register} = useForm<FormFielde>();
-
-
-  const onSubmit = (data: any) => {
-    console.log("Form Submitted:", data);
-  };
-
   return (
     <section className="flex gap-x-5 w-full items-start">
-   
-
-    
-        <form
-
-        
-          className="flex flex-col gap-2.5 w-full text-subtext-1"
-        >
-          <header className="flex justify-between items-center">
-            <h1 className="font-extrabold text-xl">اطلاعات اولیه پروفایل</h1>
-            <Button
-              type="submit"
-              className="bg-sapphire text-sm h-[36px] w-[39px] text-crust rounded"
-            >
-              ثبت
-            </Button>
-          </header>
-          <section className="bg-crust rounded-xl flex flex-col w-full">
-            <figure className="grid grid-cols-6 grid-rows-1 content-center items-center">
-              <div className="rounded-tr-xl bg-mantle w-29.5 h-full flex items-center rounded-bl-[100px] shadow-[_-1px_3px_5px_rgba(0,0,0,0.3)]">
-                <Cs12Logo className="mr-1.5" />
-              </div>
-              <img
-                src={Profile}
-                alt="User Profile"
-                className="w-20 h-20 row-start-1 col-start-2 col-end-3 mr-[25px] justify-self-start rounded-tr-[40px] rounded-bl-[40px] rounded-tl-3xl rounded-br-3xl"
-              />
-              <figcaption className="col-start-3 col-end-7 flex flex-col gap-y-2 m-2.5">
-                <TextInput
-                placeholder="نام خود را وارد کنید"
-                className="p-2 w-full rounded-md content-center bg-mantle h-13 text-[20px] font-extrabold"/>
-                  
-                
-                <div className="w-full">
-                  <TextInput
-                    name="توضیح کوتاه از فرد"
-                    placeholder="توضیح کوتاهی از خود بنویسید. در حد 200 کارکتر"
-                    className="w-full"
-                    aria-label="توضیح کوتاه از فرد"
-                  />
-                </div>
-              </figcaption>
-            </figure>
-          </section>
-
-          <section className="flex flex-col gap-y-2.5 bg-crust p-5 rounded-xl">
-            <h2 className="text-xs font-bold">
-              تنظیمات رنگ قسمت سمت راست پروفایل :
-            </h2>
-            <div className="flex flex-wrap gap-[5px]">
-              <MyRadioGroup
-                name="theme-color"
-                label="Choose your favorite color"
-                description="Select one of the color options below."
-                options={[
-                  {
-                    label: "lavender",
-                    bgColorClass: "bg-lavender",
-                    value: "lavender",
-                  },
-                  {
-                    label: "maroon",
-                    bgColorClass: "bg-maroon",
-                    value: "maroon",
-                  },
-                  { label: "teal", bgColorClass: "bg-teal", value: "teal" },
-                  {
-                    label: "peach",
-                    bgColorClass: "bg-peach",
-                    value: "peach",
-                  },
-                  { label: "sky", bgColorClass: "bg-sky", value: "sky" },
-                  {
-                    label: "mauve",
-                    bgColorClass: "bg-mauve",
-                    value: "mauve",
-                  },
-                  { label: "pink", bgColorClass: "bg-pink", value: "pink" },
-                  {
-                    label: "flamingo",
-                    bgColorClass: "bg-flamingo",
-                    value: "flamingo",
-                  },
-                ]}
-              />
+      <form className="flex flex-col gap-2.5 w-full text-subtext-1">
+        <header className="flex justify-between items-center">
+          <h1 className="font-extrabold text-xl">اطلاعات اولیه پروفایل</h1>
+          <Button
+            type="submit"
+            className="bg-sapphire text-sm h-[36px] w-[39px] text-crust rounded"
+          >
+            ثبت
+          </Button>
+        </header>
+        <section className="bg-crust rounded-xl flex flex-col w-full">
+          <figure className="grid grid-cols-6 grid-rows-1 content-center items-center">
+            <div className="rounded-tr-xl bg-mantle w-29.5 h-full flex items-center rounded-bl-[100px] shadow-[_-1px_3px_5px_rgba(0,0,0,0.3)]">
+              <Cs12Logo className="mr-1.5" />
             </div>
-          </section>
+            <Image
+              src={Profile}
+              alt="User Profile"
+              className="w-20 h-20 row-start-1 col-start-2 col-end-3 mr-[25px] justify-self-start rounded-tr-[40px] rounded-bl-[40px] rounded-tl-3xl rounded-br-3xl"
+            />
+            <figcaption className="col-start-3 col-end-7 flex flex-col gap-y-2 m-2.5">
+              <TextInput
+                placeholder="نام خود را وارد کنید"
+                className="p-2 w-full rounded-md content-center bg-mantle h-13 text-[20px] font-extrabold"
+              />
 
-          <section className="flex flex-col w-full gap-2">
-            <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
-              <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
-                <PlusSign />
-              </Button>
-              <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
-                زبان ها و تکنولوژی هایی که بلد هستید رو وارد کنید. (به زبان
-                انگلیسی) : مثال c programming language
-              </Text>
-            </article>
-
-            <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
-              <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
-                <PlusSign />
-              </Button>
-              <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
-                لینک سوشال میدیا هایی که دارید رو اینجا بزارید.
-              </Text>
-            </article>
-
-            <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl w-full">
               <div className="w-full">
                 <TextInput
-                  name="نظر در مورد سایت"
-                  placeholder="نظرتون رو در مورد این سایت بنویسید. (بعد از ۲۰ روز در این قسمت امکان نوشتن هست)"
+                  name="توضیح کوتاه از فرد"
+                  placeholder="توضیح کوتاهی از خود بنویسید. در حد 200 کارکتر"
                   className="w-full"
-                  aria-label="نظر در مورد سایت"
+                  aria-label="توضیح کوتاه از فرد"
                 />
               </div>
-            </article>
-          </section>
+            </figcaption>
+          </figure>
+        </section>
 
-          <section>
-            <h2 className="font-extrabold text-xl">تنظیمات</h2>
-            <SettingCheckboxGroup
-              name="تنظیمات"
-              className="bg-crust rounded-xl p-2 w-full flex flex-col gap-y-2.5"
-            >
-              <SettingCheckboxOption value="sss">
-                آیا ۱۰ تا از آخرین نظراتتون در صفحه پروفایلتون توسط دیگران دیده
-                شود؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="sss1">
-                آیا قسمت زبان‌هایی که بلدید در صفحه پروفایلتون توسط دیگران دیده
-                شود؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="sss2">
-                آیا قسمت هایلایت‌ها و یادداشت‌های شیر شده توسط شما در قسمت
-                پروفایلتون قابل مشاهده باشد؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="sss3">
-                آیا قسمت سوشال مدیای شما در پروفایلتون قابل مشاهده توسط دیگران
-                باشد؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="sss4">
-                آیا تصویر پروفایل شما برای دیگران قابل مشاهده باشد؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="ss5">
-                آیا امتیاز و جایگاه شما در چالش‌های هفتگی قابل مشاهده باشد؟
-              </SettingCheckboxOption>
-              <SettingCheckboxOption value="sss6">
-                آیا صفحه پروفایل برای شما ساخته شود؟
-              </SettingCheckboxOption>
-            </SettingCheckboxGroup>
-          </section>
+        <section className="flex flex-col gap-y-2.5 bg-crust p-5 rounded-xl">
+          <h2 className="text-xs font-bold">
+            تنظیمات رنگ قسمت سمت راست پروفایل :
+          </h2>
+          <div className="flex flex-wrap gap-[5px]">
+            <MyRadioGroup
+              name="theme-color"
+              options={[
+                {
+                  label: "lavender",
+                  bgColorClass: "bg-lavender",
+                  value: "lavender",
+                },
+                {
+                  label: "maroon",
+                  bgColorClass: "bg-maroon",
+                  value: "maroon",
+                },
+                { label: "teal", bgColorClass: "bg-teal", value: "teal" },
+                {
+                  label: "peach",
+                  bgColorClass: "bg-peach",
+                  value: "peach",
+                },
+                { label: "sky", bgColorClass: "bg-sky", value: "sky" },
+                {
+                  label: "mauve",
+                  bgColorClass: "bg-mauve",
+                  value: "mauve",
+                },
+                { label: "pink", bgColorClass: "bg-pink", value: "pink" },
+                {
+                  label: "flamingo",
+                  bgColorClass: "bg-flamingo",
+                  value: "flamingo",
+                },
+              ]}
+            />
+          </div>
+        </section>
 
-          <section>
-            <h2 className="font-extrabold text-xl">درخواست ها</h2>
-            <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
-              <Text className="bg-mantle p-2 w-full rounded-md h-9 text-xs">
-                شما یک کاربر عادی هستید برای تبدیل شدن به منتور درخواست بدید.
-              </Text>
-            </article>
-          </section>
+        <section className="flex flex-col w-full gap-2">
+          <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
+            <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
+              <PlusSign />
+            </Button>
+            <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
+              زبان ها و تکنولوژی هایی که بلد هستید رو وارد کنید. (به زبان
+              انگلیسی) : مثال c programming language
+            </Text>
+          </article>
 
-          <section>
-            <h2 className="font-extrabold text-xl">تنظیمات اولیه</h2>
-            <div className="f">
-              <fieldset className="grid grid-cols-4 gap-2.5 bg-crust p-2 rounded-xl">
-                <div className="col-start-1 col-end-3">
-                  <TextInput
-                    name="نام کاربری"
-                    placeholder="نام کاربری"
-                    aria-label="نام کاربری"
-                  />
-                </div>
-                <div className="col-start-3 col-end-5">
-                  <TextInput
-                    name="رمز عبور"
-                    placeholder="رمز عبور"
-                    aria-label="رمز عبور"
-                  />
-                </div>
-                <div className="col-span-4">
-                  <TextInput
-                    name="ایمیل"
-                    placeholder="ایمیل"
-                    aria-label="ایمیل"
-                  />
-                </div>
-                <div className="col-span-4">
-                  <Button className="bg-crust border border-red border-2 w-full p-2 h-9 text-red text-xs">
-                    تغییر رمز عبور
-                  </Button>
-                </div>
-              </fieldset>
+          <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
+            <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
+              <PlusSign />
+            </Button>
+            <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
+              لینک سوشال میدیا هایی که دارید رو اینجا بزارید.
+            </Text>
+          </article>
 
-              <div className="grid grid-cols-11 gap-2.5 text-xs font-bold text-crust">
-                <div className="col-span-8">
-                  <Button className="bg-sapphire w-full p-2 h-9">ثبت</Button>
-                </div>
-                <div className="col-span-3">
-                  <Button className="bg-maroon w-full p-2 h-9">
-                    بازگشت به تنظیمات پیشفرض
-                  </Button>
-                </div>
+          <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl w-full">
+            <div className="w-full">
+              <TextInput
+                name="نظر در مورد سایت"
+                placeholder="نظرتون رو در مورد این سایت بنویسید. (بعد از ۲۰ روز در این قسمت امکان نوشتن هست)"
+                className="w-full"
+                aria-label="نظر در مورد سایت"
+              />
+            </div>
+          </article>
+        </section>
+
+        <section>
+          <h2 className="font-extrabold text-xl">تنظیمات</h2>
+          <SettingCheckboxGroup
+            name="تنظیمات"
+            className="bg-crust rounded-xl p-2 w-full flex flex-col gap-y-2.5"
+          >
+            <SettingCheckboxOption value="sss">
+              آیا ۱۰ تا از آخرین نظراتتون در صفحه پروفایلتون توسط دیگران دیده
+              شود؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="sss1">
+              آیا قسمت زبان‌هایی که بلدید در صفحه پروفایلتون توسط دیگران دیده
+              شود؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="sss2">
+              آیا قسمت هایلایت‌ها و یادداشت‌های شیر شده توسط شما در قسمت
+              پروفایلتون قابل مشاهده باشد؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="sss3">
+              آیا قسمت سوشال مدیای شما در پروفایلتون قابل مشاهده توسط دیگران
+              باشد؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="sss4">
+              آیا تصویر پروفایل شما برای دیگران قابل مشاهده باشد؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="ss5">
+              آیا امتیاز و جایگاه شما در چالش‌های هفتگی قابل مشاهده باشد؟
+            </SettingCheckboxOption>
+            <SettingCheckboxOption value="sss6">
+              آیا صفحه پروفایل برای شما ساخته شود؟
+            </SettingCheckboxOption>
+          </SettingCheckboxGroup>
+        </section>
+
+        <section>
+          <h2 className="font-extrabold text-xl">درخواست ها</h2>
+          <article className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
+            <Text className="bg-mantle p-2 w-full rounded-md h-9 text-xs">
+              شما یک کاربر عادی هستید برای تبدیل شدن به منتور درخواست بدید.
+            </Text>
+          </article>
+        </section>
+
+        <section>
+          <h2 className="font-extrabold text-xl">تنظیمات اولیه</h2>
+          <div className="f">
+            <fieldset className="grid grid-cols-4 gap-2.5 bg-crust p-2 rounded-xl">
+              <div className="col-start-1 col-end-3">
+                <TextInput
+                  name="نام کاربری"
+                  placeholder="نام کاربری"
+                  aria-label="نام کاربری"
+                />
+              </div>
+              <div className="col-start-3 col-end-5">
+                <TextInput
+                  name="رمز عبور"
+                  placeholder="رمز عبور"
+                  aria-label="رمز عبور"
+                />
+              </div>
+              <div className="col-span-4">
+                <TextInput
+                  name="ایمیل"
+                  placeholder="ایمیل"
+                  aria-label="ایمیل"
+                />
+              </div>
+              <div className="col-span-4">
+                <Button className="bg-crust border border-red border-2 w-full p-2 h-9 text-red text-xs">
+                  تغییر رمز عبور
+                </Button>
+              </div>
+            </fieldset>
+
+            <div className="grid grid-cols-11 gap-2.5 text-xs font-bold text-crust">
+              <div className="col-span-8">
+                <Button className="bg-sapphire w-full p-2 h-9">ثبت</Button>
+              </div>
+              <div className="col-span-3">
+                <Button className="bg-maroon w-full p-2 h-9">
+                  بازگشت به تنظیمات پیشفرض
+                </Button>
               </div>
             </div>
-          </section>
-        </form>
-      
+          </div>
+        </section>
+      </form>
     </section>
   );
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,7 +1,6 @@
 import axios from "~/lib/axios";
 
 export const getUser = async () => {
-    const res = await axios.get("/api/user-profiles");
-    return res.data;
- };
-
+  const res = await axios.get("/api/user-profiles");
+  return res.data;
+};
