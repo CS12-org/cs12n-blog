06f67031cef877f2c75bc7d183802c8125f33851
zahra-sarabi <zahrasarabi1379@gmail.com>
2025-09-10 20:21:35 +0330

    fix: handle the problem

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -4,46 +4,6 @@ import axios from '~/lib/axios';
  * Types for paginated posts
  * ---------------------------- */
 export type GetPostsParams = { page: number; pageSize: number };
-
-export type GetPostsResult = {
-  data: {
-    id: number;
-    description: string;
-    title: string;
-    content: string;
-    clap: number;
-    slug: string;
-    createdAt: string;
-    narrator: { url: string } | null;
-    user: { email: string; username: string } | null;
-    tags: { id: number; title: string; slug: string }[];
-    category: { id: number; slug: string; title: string };
-    featured_image: {
-      id: number;
-      width: number;
-      height: number;
-      url: string;
-    } | null;
-    slide_image:
-      | { id: number; width: number; height: number; url: string }[]
-      | null;
-  }[];
-  meta: {
-    pagination: {
-      page: number;
-      pageSize: number;
-      total: number;
-      pageCount: number;
-    };
-  };
-};
-
-/** Fetch paginated posts */
-export const getPosts = (params: GetPostsParams) =>
-  axios.get<GetPostsResult>("/api/posts/feed", { params });
-
-export type PostItem = GetPostsResult["data"][0];
-
 /** ----------------------------
  * Type for single post (simplified)
  * ---------------------------- */
