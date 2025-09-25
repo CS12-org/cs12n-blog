3e85f435fece544ae477e7fc88e919557a4b03c7
zahra-sarabi <zahrasarabi1379@gmail.com>
2025-09-25 14:24:57 +0330

    handel the text editor

--- src/components/shared/text-editor-input/text-editor-input.tsx
+++ src/components/shared/text-editor-input/text-editor-input.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC } from 'react';
+
 import { useEditor, EditorContent } from '@tiptap/react';
 import Bold from '@tiptap/extension-bold';
 import Document from '@tiptap/extension-document';
@@ -14,15 +14,11 @@ import { FaCode } from 'react-icons/fa6';
 import { FaItalic } from 'react-icons/fa6';
 import { HiOutlineBold } from 'react-icons/hi2';
 import { TextEditorButton } from './text-editor-button';
-import { Placeholder } from '@tiptap/extension-placeholder';
+import { FC } from 'react';
 
-type TextEditorInputProps = { content?: string; placeHolder?: string; onChange?: (content: string) => void };
+type TextEditorInputProps = { content?: string; placeHolder?: string };
 
-const TextEditorInput: FC<TextEditorInputProps> = ({
-  content = '',
-  placeHolder = 'کامنت خود رو بنویسید ...',
-  onChange,
-}) => {
+const TextEditorInput: FC<TextEditorInputProps> = ({ content = '', placeHolder = 'کامنت خود رو بنویسید ...' }) => {
   const lowlight = createLowlight(all);
   const editor = useEditor({
     extensions: [
@@ -35,28 +31,17 @@ const TextEditorInput: FC<TextEditorInputProps> = ({
       CodeBlockLowlight.configure({
         lowlight,
       }),
-      Placeholder.configure({
-        placeholder: placeHolder, // Use the placeHolder prop
-        showOnlyWhenEditable: true, // Show placeholder only when editor is editable
-        showOnlyCurrent: false, // Show placeholder in all empty nodes
-      }),
     ],
-    content: content.trim() || '<p></p>',
+    content: `${content?.trim() === '' ? `<p> ${placeHolder} </p>` : content}`,
     immediatelyRender: false,
     shouldRerenderOnTransaction: true,
-    onUpdate: ({ editor }) => {
-      // Trigger onChange with the editor's HTML content
-      if (onChange) {
-        onChange(editor.getHTML());
-      }
-    },
   });
   if (!editor) {
     return null;
   }
 
   return (
-    <div className="bg-surface-0 flex flex-col gap-4 rounded-xl p-2">
+    <div className="flex flex-col gap-4">
       <EditorContent editor={editor} />
       <div className="flex gap-2 self-end">
         <TextEditorButton
