0417cfa0523a566897b72a0d07b55a133e6beabc
mohammadhosseintazaroie <m.h.tazaroie753@gmail.com>
2025-10-06 20:40:09 +0330

    refactor: restore and enhance ChangePassword component with metadata and form integration

--- src/app/(auth)/change-password/page.tsx
+++ src/app/(auth)/change-password/page.tsx
@@ -1,17 +1,17 @@
-// import type { Metadata } from 'next';
-// import ChangePasswordForm from '~/components/Auth/change-password-form';
+import type { Metadata } from 'next';
+import ChangePasswordForm from '@/components/auth/change-password-form';
 
-// export const metadata: Metadata = {
-//   title: 'تغییر رمز عبور',
-//   description: 'تغییر رمز عبور با وارد کردن رمز جدید و تکرار آن',
-//   keywords: ['تغییر رمز عبور', 'رمز جدید', 'تکرار رمز', 'امنیت'],
-//   openGraph: {
-//     title: 'تغییر رمز عبور',
-//     description: 'تغییر رمز عبور با وارد کردن رمز جدید و تکرار آن',
-//     type: 'website',
-//   },
-// };
+export const metadata: Metadata = {
+  title: 'تغییر رمز عبور',
+  description: 'تغییر رمز عبور با وارد کردن رمز جدید و تکرار آن',
+  keywords: ['تغییر رمز عبور', 'رمز جدید', 'تکرار رمز', 'امنیت'],
+  openGraph: {
+    title: 'تغییر رمز عبور',
+    description: 'تغییر رمز عبور با وارد کردن رمز جدید و تکرار آن',
+    type: 'website',
+  },
+};
 
-// export default function ChangePassword() {
-//   return <ChangePasswordForm />;
-// }
+export default function ChangePassword() {
+  return <ChangePasswordForm />;
+}
